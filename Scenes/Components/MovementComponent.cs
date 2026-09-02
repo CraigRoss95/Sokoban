33f9837151cd4359cea9cef31d70362ca662d9a2
Craig Ross <craigross.emailme@gmail.com>
2026-09-01 21:22:26 -0400

    fix player walking through blocks

--- Scenes/Components/MovementComponent.cs
+++ Scenes/Components/MovementComponent.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 
 [Tool]
@@ -7,7 +6,11 @@ using System;
 public partial class MovementComponent : Node2D
 
 {
-	[Export] public RayCast2D checkDirectionRayCast;
+	[Export] public RayCast2D rayCastUp;
+	[Export] public RayCast2D rayCastRight;
+	[Export] public RayCast2D rayCastDown;
+	[Export] public RayCast2D rayCastLeft;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -28,11 +31,10 @@ public partial class MovementComponent : Node2D
 
 	public bool CanMove(Vector2 direction)
 	{
-		//TODO IMPORTANT This line is running on the same frame as input so the player isn't rotating
-		checkDirectionRayCast.Rotation = direction.Angle();
-		if (checkDirectionRayCast.IsColliding())
+		RayCast2D ray = GetRay(direction);
+		if (ray.IsColliding())
 		{
-			Node collision = (Node)checkDirectionRayCast.GetCollider();
+			Node collision = (Node)ray.GetCollider();
 			GD.Print("impact " + GetParent().Name + collision.GetParent().Name);
 			return false;
 		}
@@ -41,4 +43,24 @@ public partial class MovementComponent : Node2D
 		return true;
 	}
 	
+	private RayCast2D GetRay (Vector2 direction)
+	{
+		//Note Raycast Up is at the bottom (defualt)
+		if (direction == Vector2.Right)
+		{
+			return rayCastRight;
+		}
+		else if (direction == Vector2.Down)
+		{
+			return rayCastDown;
+		}
+		else if (direction == Vector2.Left)
+		{
+			return rayCastLeft;
+		}
+		else
+		{
+			return rayCastUp;
+		}
+	}
 }
