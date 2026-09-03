c47c4ef18b770242d3cd0f269166bbd394cb891b
Craig Ross <craigross.emailme@gmail.com>
2026-09-02 21:23:07 -0400

    remove input buffers, too slow

--- Scenes/Components/PlayerControllerComponent.cs
+++ Scenes/Components/PlayerControllerComponent.cs
@@ -11,20 +11,8 @@ public partial class PlayerControllerComponent : Node2D
 	[Export] MovementComponent movementComponent;
 	[Export] AdjacentRayComponent	adjacentRayComponent;
 
-	[Export] Timer inputBufferTimer;
-
-	private bool bufferInputs = false;
-	
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		inputBufferTimer.Timeout += StopBufferingInput;
-	}
-
-    public override void _ExitTree()
-    {
-        inputBufferTimer.Timeout -= StopBufferingInput;
-    }
+	
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,20 +32,14 @@ public partial class PlayerControllerComponent : Node2D
 		//Move and buffer for input
 		Vector2 inputDirection = Input.GetVector("left","right","up","down");
 
-		if (bufferInputs == false &&
+		if (
 			Global.directionList.Contains(inputDirection))
 		{
-			StartBufferInput();
 			UseInput(inputDirection);
 		}
 
-		//TODO add smear lines if this if this triggers in quick succession
-		//Release Buffer if movment key is released
-		if (bufferInputs == true 
-		&& inputDirection == new Vector2())
-		{
-			StopBufferingInput();
-		}
+		//TODO add smear lines if this if this triggers
+		
 	}
 
 	private void Push (Vector2 direction)
@@ -65,7 +47,7 @@ public partial class PlayerControllerComponent : Node2D
 		Node2D pushObject = adjacentRayComponent.GetAdjacentNode(direction);
 		GD.Print(pushObject.Name);
 
-		if (pushObject.HasNode("MovementComponent"))
+		if (!pushObject.HasNode("MovementComponent"))
 		{
 			return;
 		}
@@ -81,14 +63,6 @@ public partial class PlayerControllerComponent : Node2D
 			//TODO Play sad sound
 		}
 
-	}
-
-	private void StopBufferingInput() {bufferInputs = false;}
-
-	private void StartBufferInput ()
-	{
-		bufferInputs = true;
-		inputBufferTimer.Start();
 	}
 
 	private void UseInput(Vector2 inputDirection)
